53628e302c22de0a7325727792a1cdafeaa9473d
Jack Zheng <jzheng7@stuy.edu>
2018-02-14 18:35:36 -0500

    Just pushing.

--- practice1.py
+++ practice1.py
@@ -5,15 +5,34 @@ Created on Mon Feb 12 14:02:28 2018
 
 @author: Zhoukx.joseph
 """
-from flask import Flask, render_template
+from flask import Flask, render_template, request, json, jsonify, redirect
+
+todo = []
 
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return render_template("index.html", todo = todo)
 
+@app.route('/todo/create', methods=['POST'])
+def createItem():
+    newItem = request.form['myInput']
+    todo.append(newItem)
+    return jsonify(todo)
+    # return redirect('/')
 
-def home():
-    return render_template('index.html')
+@app.route('/todo/read')
+def fetchItems():
+    return jsonify(todo)
+
+# @app.route('/todo/update', methods=['PUT'])
+# def updateItems():
+#     pass
+#
+# @app.route('/todo/delete', methods=['DETELE'])
+# def deteleItem():
+#     pass
 
 if __name__ == '__main__':
     app.run(debug=True)
