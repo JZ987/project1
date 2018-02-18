edb83bbe59ba46503be9912bab83d9e7e53a911b
Kaixuan Zhou <zhoukx.joseph@gmail.com>
2018-02-18 18:17:35 -0500

    correct issues of "app.py"

--- app.py
+++ app.py
@@ -2,17 +2,15 @@ from flask import Flask, render_template, request, jsonify, session
 import json
 
 todo = []
-
 app = Flask(__name__)
-
 app.secret_key = "the_secrete_key_which!.di!345"
 
 @app.route("/")
-def index():
+def home():
 	return render_template("home.html")
 
 @app.route("/todo/create", methods=["POST"])
-def addToDo():
+def createItem():
     newItem = request.form["InputData"]
     if(newItem !=""):
         todo.append(newItem)
@@ -21,16 +19,16 @@ def addToDo():
     return jsonify({"error" : "no input"})
 
 @app.route("/todo/read")
-def getToDo():
+def getList():
 	session["user"] = todo
 	return jsonify(session["user"])
 
 @app.route("/todo/delete", methods=["DELETE"])
-def deleteToDo():
+def deleteItem():
 	Deleted = request.data.decode("utf-8")
 	global todo
-	todo = [x for x in todo if x != Deleted]
-	session["user"] = todo
+	todo = [ x for x in todo if x != Deleted]
+	session['user']=todo
 	return jsonify(session["user"])
 
 if __name__ == "__main__":
