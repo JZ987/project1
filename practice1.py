54044d0e1e45579c633fd5874115912b08fa2065
Jack Zheng <jzheng7@stuy.edu>
2018-02-19 03:24:43 -0500

    Implement all the necessary methods

--- practice1.py
+++ practice1.py
@@ -9,30 +9,35 @@ from flask import Flask, render_template, request, json, session, jsonify, redir
 
 todo = []
 app = Flask(__name__)
-app.secret_key='the_secrete_key_which!.di!345'
+app.secret_key = "\xa0\x1f\x02v#\xa1\x1e;\xfc\x0f/\x13Cz\x0e\x91\x1e\x08\x9a5\xb0Je\x01"
 
 @app.route('/')
 def index():
-    # session['username'] = []
+    session['username'] = todo
     return render_template("index.html")
 
 @app.route('/todo/create', methods=['POST'])
 def createItem():
     newItem = request.form['newItem']
     todo.append(newItem)
-    return jsonify({"result" : todo[-1]})
+    session['username'] = todo
+    return jsonify({"result" : "success"})
 
 @app.route('/todo/read')
 def fetchItems():
-    return jsonify(todo)
+    return jsonify({"result" : "success", "data" : session['username']})
 
-# @app.route('/todo/update', methods=['PUT'])
-# def updateItems():
-#     pass
+@app.route('/todo/update', methods=['PUT'])
+def updateItems():
+    index = todo.index(request.form['old'])
+    todo[index] = request.form['item']
+    session['username'] = todo
+    return jsonify({"result" : "success"})
 
 @app.route('/todo/delete', methods=['DELETE'])
 def deteleItem():
     todo.remove(request.form['item'])
+    session['username'] = todo
     return jsonify({"result" : "success"})
 
 if __name__ == '__main__':
