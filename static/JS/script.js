f7ff0b6029d4d5aabb943a30ecfc13c6a315d698
Jack Zheng <jzheng7@stuy.edu>
2018-02-19 20:44:33 -0500

    Move all script to script.js from index.html

--- static/JS/script.js
+++ static/JS/script.js
@@ -1,56 +1,98 @@
-// function addToList() {
-//   var inputValue = $('#myInput').val();
-//   if (inputValue == "") {
-//     alert("You must write something!");
-//   } else {
-//     var li = document.createElement("li");
-//     li.appendChild(document.createTextNode(inputValue));
-//     var span = document.createElement("span");
-//     span.appendChild(document.createTextNode("\u00D7"));
-//     span.className = 'close';
-//     li.appendChild(span);
-//     $("#myUL").append(li);
-//     $('#myInput').val("");
-//   }
-// }
-
-function checkOff() {
-  $('#myUL').click(function(e) {
-    if (e.target.tagName === 'LI') {
-      e.target.classList.toggle("checked");
-    }
-  });
-}
-
-function removeFromList() {
-  $("#myUL").click(function(e) {
-    if (e.target.tagName === 'SPAN') {
-      e.target.parentElement.remove();
-    }
-  })
-}
+var oriText = "";
 
 function saveData() {
-  var newItem = $('#myInput').val()
+  var newItem = $('#myInput').val();
   if (newItem == "") {
     alert("You must write something!");
   } else {
-    $.ajax({
-      url: '/todo/create',
-      type: 'POST',
-      data: $('form').serialize(),
-      success: function(response){
-				console.log(response);
-			},
-			error: function(error){
-				console.log(error);
-			}
-    });
+    var url = "/todo/create";
+    var settings = {type : "POST",
+                    data : {"newItem" : newItem},
+                    success: function(response) {
+                      var li = $("<li>" + newItem + "</li>");
+                      var span = $("<span class='close'>×</span>");
+                      $('#myUL').append(li.append(span));
+                    },
+                    error: function(error) {
+                      console.log(error);
+                    }};
+    $.ajax(url, settings);
     $('#myInput').val("");
   }
 }
 
+function deleteItem() {
+  $("#myUL").on('click', 'span', function () {
+    var task = $(this).parent().text().slice(0, -1);
+    var url = "/todo/delete";
+    var settings = {type : "DELETE",
+                    data : {item : task},
+                    success : function(response) {
+                      oriText = "";
+                      console.log(response.result);
+                    },
+                    error : function(error) {
+                      console.log(error);
+                    }};
+    $.ajax(url, settings);
+    $(this).parent().remove();
+  });
+}
+
+function getList() {
+  var url = "/todo/read";
+  var settings = {type : "GET",
+                  success : function(response) {
+                    for(var i = 0; i < response.data.length; i++) {
+                      var li = $("<li>" + response.data[i] + "</li>");
+                      var span = $("<span class='close'>×</span>");
+                      $('#myUL').append(li.append(span));
+                    }
+                  },
+                  error : function(error) {
+                    console.log(error);
+                  }};
+  $.ajax(url, settings);
+}
+
+function modifyItem() {
+  $("#myUL").on('click', 'li' , function() {
+    if (oriText == "") {
+      oriText = $(this).text().slice(0, -1);
+      $(this).text("");
+      $("<input id='newContent'type='text'>").appendTo(this).focus().val(oriText);
+    }
+  });
+  $("#myUL").on('keypress', 'input', function (e) {
+    if (e.keyCode == 13) {
+      if ($("#newContent").val() == "") {
+        li = oriText + "<span class='close'>×</span>";
+      } else {
+        var task = $("#newContent").val()
+        li = task + "<span class='close'>×</span>";
+        var url = "/todo/update";
+        var settings = {type : "PUT",
+                        data : {item : task, old : oriText},
+                        success : function(response) {
+                          console.log(response.result);
+                        },
+                        error : function(error) {
+                          console.log(error);
+                        }};
+        $.ajax(url, settings);
+      }
+      $(this).parent().html(li);
+      oriText = "";
+    }
+  });
+  $("#myUL").on('focusout', 'li > input', function() {
+    $(this).parent().html(oriText + "<span class='close'>×</span>");
+    oriText = "";
+  });
+}
+
 function setup() {
+  getList();
   $('#add').click(saveData);
   $('#myInput').keypress(function(e){
       if(e.keyCode == 13){
@@ -58,8 +100,8 @@ function setup() {
         saveData();
       }
   });
-  checkOff();
-  removeFromList();
+  deleteItem();
+  modifyItem();
 }
 
 $(document).ready(setup);
