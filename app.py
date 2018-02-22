2234a3e87b760c9081bfbc9b97c39ad087119600
Kaixuan Zhou <zhoukx.joseph@gmail.com>
2018-02-21 23:35:02 -0500

    Create DataBase / UPDATE

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
 from flask import Flask, render_template, request, jsonify, session
+import pymysql.cursors
 import json
+
 
 todo = []
 app = Flask(__name__)
 app.secret_key = "the_secrete_key_which!.di!345"
+
+
+conn = pymysql.connect(host = 'localhost', 
+					   user='username', 
+					   password='cs1122project',
+					   database='todo'
+					   )
+# conn.cursor().execute('CREATE DATABASE todo')
+# conn.cursor().execute("CREATE TABLE todolist (id smallint unsigned not null auto_increment, things varchar(65) not null, primary key (id));")
+
+try:
+    with conn.cursor() as cursor:
+    	sql = "SELECT * FROM todolist"
+    	cursor.execute(sql)
+    	result = cursor.fetchall()
+    	print(result)
+
+finally:
+	print("connect successfully")
 
 @app.route("/")
 def home():
@@ -15,7 +36,13 @@ def createItem():
     if(newItem !=""):
         todo.append(newItem)
         session["user"] = todo
-        return jsonify({"success" : session["user"][-1]})
+        try:
+    		with conn.cursor() as cursor:
+        		sql = "INSERT INTO `todolist` (`things`) VALUES (%s)"
+        		cursor.execute(sql, (newItem))
+    		conn.commit()
+    	finally:
+    		return jsonify({"success" : session["user"][-1]})
     return jsonify({"error" : "no input"})
 
 @app.route("/todo/read")
