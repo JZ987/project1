e5bc799e98492889e54edd8ae81af08ba49fc4a5
Jack Zheng <jzheng7@stuy.edu>
2018-02-25 23:37:09 -0500

    Ready for merging

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import json
 
 app = Flask(__name__)
 
-conn = pymysql.connect(host = 'localhost', 
-					   user='username', 
+conn = pymysql.connect(host = 'localhost',
+					   user='username',
 					   password='cs1122project',
 					   database='todo')
 # conn.cursor().execute('CREATE DATABASE todo')
