fb1b6c90b574ded13ec2e91d01d48b6336290060
Kaixuan Zhou <zhoukx.joseph@gmail.com>
2018-02-25 19:44:01 -0500

    Adding database/UPDATE STILL NEEDS SOME FIX

--- app.py
+++ app.py
@@ -1,18 +1,13 @@
-from flask import Flask, render_template, request, jsonify, session
+from flask import Flask, request, jsonify, render_template
 import pymysql.cursors
 import json
 
-
-todo = []
 app = Flask(__name__)
-app.secret_key = "the_secrete_key_which!.di!345"
-
 
 conn = pymysql.connect(host = 'localhost', 
 					   user='username', 
 					   password='cs1122project',
-					   database='todo'
-					   )
+					   database='todo')
 # conn.cursor().execute('CREATE DATABASE todo')
 # conn.cursor().execute("CREATE TABLE todolist (id smallint unsigned not null auto_increment, things varchar(65) not null, primary key (id));")
 
@@ -34,38 +29,50 @@ def home():
 def createItem():
     newItem = request.form["InputData"]
     if(newItem !=""):
-        todo.append(newItem)
-        session["user"] = todo
         try:
     		with conn.cursor() as cursor:
         		sql = "INSERT INTO `todolist` (`things`) VALUES (%s)"
         		cursor.execute(sql, (newItem))
+        		priKey = cursor.lastrowid
     		conn.commit()
     	finally:
-    		return jsonify({"success" : session["user"][-1]})
+    		return jsonify({"success" : newItem, 'key' : priKey})
     return jsonify({"error" : "no input"})
 
 @app.route("/todo/read")
 def getList():
-	session["user"] = todo
-	return jsonify(session["user"])
+	try:
+	    with conn.cursor() as cursor:
+	    	sql = "SELECT * FROM todolist"
+	    	cursor.execute(sql)
+	    	result = cursor.fetchall()
+	finally:
+		return jsonify(result)
 
 @app.route("/todo/delete", methods=["DELETE"])
 def deleteItem():
-	Deleted = request.data.decode("utf-8")
-	global todo
-	todo = [ x for x in todo if x != Deleted]
-	session['user']=todo
-	return jsonify({'success' : Deleted})
-
+	Deleted = request.form["data"]
+	key = request.form["key"]
+	try:
+	    with conn.cursor() as cursor:
+	    	sql = "DELETE FROM todolist WHERE id=(%s)"
+	    	cursor.execute(sql,(key))
+	    conn.commit()
+	finally:
+		return jsonify({'success' : Deleted})
 
 @app.route("/todo/update", methods=["PUT"])
 def updateItem():
 	Old = request.form['old']
 	New = request.form['item']
-	todo[todo.index(Old)] = New
-	session['user']=todo
-	return jsonify({"success" : New})
+	key = request.form['key']
+	try:
+	    with conn.cursor() as cursor:
+	    	sql = "UPDATE todolist SET things =(%s) WHERE id=(%s)"
+	    	cursor.execute(sql,(New, key))
+	    conn.commit()
+	finally:
+		return jsonify({'success' : New})
 
 
 if __name__ == "__main__":
--- static/JS/main.js
+++ static/JS/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     var oriText = "";
+    var keys = 0;
     var $MyList = $("#MyList");
     $("form").on("submit", function(event) {
         $.ajax({
@@ -14,14 +15,13 @@ $(document).ready(function() {
                 alert("You must write something");
             } else {
                 console.log(data.success);
-                var li = document.createElement("li");
-                li.appendChild(document.createTextNode(data.success));
-                var span = document.createElement("span");
-                span.appendChild(document.createTextNode("\u00D7"));
-                span.className = 'close';
-                li.appendChild(span);
-
-                $MyList.append(li);
+                var li = $("<li>" + data.success + "</li>");
+                var span = $("<span class='close'>×</span>");
+                var key = $("<p style='display:none;'>" + data.key + "</p>");
+                var duplicate = $("<p style='display:none;'>" + data.success + "</p>");
+                li.append(duplicate);
+                li.append(key);
+                $MyList.append(li.append(span));
                 $("#myInput").val("");
             }
         });
@@ -36,62 +36,57 @@ $(document).ready(function() {
     })
     .done(function(todo) {
         $.each(todo, function(index, item) { 
-                var li = document.createElement("li");
-                li.appendChild(document.createTextNode(item));
-                var span = document.createElement("span");
-                span.appendChild(document.createTextNode("\u00D7"));
-                span.className = 'close';
-                li.appendChild(span);
-                $MyList.append(li);
+                var li = $("<li>" + item[1] + "</li>");
+                var duplicate = $("<p style='display:none;'>" + item[1] + "</p>");
+                var span = $("<span class='close'>×</span>");
+                var key = $("<p style='display:none;'>" + item[0] + "</p>");
+                li.append(duplicate);
+                li.append(key);
+                $MyList.append(li.append(span));
         });
-
     });
 
-    $("#MyList").on("click", ".close", function() {
-        var task = $(this).parent().html().slice(0,-28);
-        //$(this).parent().find('li').last().remove();
-        $(this).parent().remove();
-        console.log(task);
-        $.ajax({
-            data: task,
-            contentType: "application/json; charset=utf-8",
-            type: "DELETE",
-            url: "/todo/delete"
-        })
-        .done(function(data) {
-            // $("#MyList").empty();
-            // $.each(data, function(index, item) { 
-            //     var li = document.createElement("li");
-            //     li.appendChild(document.createTextNode(item));
-            //     var span = document.createElement("span");
-            //     span.appendChild(document.createTextNode("\u00D7"));
-            //     span.className = 'close';
-            //     li.appendChild(span);
-            //     $MyList.append(li);
-            // });
-            console.log(data.success);
-        });
-        event.preventDefault();
+
+
+    $("#MyList").on('click', '.close', function () {
+      var task = $(this).parent().children();
+      var url = "/todo/delete";
+      var settings = {type : "DELETE",
+                      data : {data : task[0].innerHTML, key:task[1].innerHTML,},
+                      success : function(response) {
+                        oriText = "";
+                        console.log(response.result);
+                      },
+                      error : function(error) {
+                        console.log(error);
+                      }};
+      $.ajax(url, settings);
+      $(this).parent().remove();
     });
+
+
+
 
 
     $("#MyList").on('click', 'li' , function() {
       if (oriText == "") {
-        oriText = $(this).text().slice(0, -1);
+        var task = $(this).children();
+        oriText = task[0].innerHTML;
+        keys = task[1].innerHTML;
         $(this).text("");
-        $("<input id='newContent'type='text' style='width:50%;background:rgba(255,255,255,0.7);text-color:white;'>").appendTo(this).focus().val(oriText);
+        $("<input id='newContent'type='text' style='background:transparent;color:white;'>").appendTo(this).focus().val(oriText);
       }
     });
     $("#MyList").on('keypress', 'input', function (e) {
       if (e.keyCode == 13) {
         if ($("#newContent").val() == "") {
-          li = oriText + "<span class='close'>×</span>";
+          $(this).parent().html(oriText + "<p style='display:none;'>" + oriText + "</p>" +"<p style='display:none;'>" + keys + "</p>" + "<span class='close'>×</span>");
         } else {
           var task = $("#newContent").val()
-          li = task + "<span class='close'>×</span>";
+          $(this).parent().html(task + "<p style='display:none;'>" + task + "</p>" +"<p style='display:none;'>" + keys + "</p>" + "<span class='close'>×</span>");
           var url = "/todo/update";
           var settings = {type : "PUT",
-                          data : {item : task, old : oriText},
+                          data : {item : task, old : oriText, key : keys},
                           success : function(response) {
                             console.log(response.success);
                           },
@@ -100,21 +95,9 @@ $(document).ready(function() {
                           }};
           $.ajax(url, settings);
         }
-        $(this).parent().html(li);
         oriText = "";
       }
     });
-    $("#MyList").on('focusout', 'li > input', function() {
-      $(this).parent().html(oriText + "<span class='close'>×</span>");
-      oriText = "";
-    });
-
-
-
-
-
-
-
 
 
 });
